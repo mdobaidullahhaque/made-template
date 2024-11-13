74346253c8ff19405dfecfab2c4fcfef20ac1070
Md. Obaidullah Haque <mdobaidullahhaque.cse@gmail.com>
2024-11-13 19:37:27 +0100

    Update pipeline.py

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,29 +1,18 @@
 !pip install opendatasets
 
-import kaggle
-import zipfile
-import os
 import opendatasets as od
+import os
+import pandas as pd
 
-
+#Kaggle dataset download path
 
 dataset = 'https://www.kaggle.com/datasets/mjshri23/life-expectancy-and-socio-economic-world-bank?select=life+expectancy.csv'
 od.download(dataset)
 
 
+data_dir = '.\life-expectancy-and-socio-economic-world-bank'
+os.listdir(data_dir)
 
-# Define Kaggle dataset and download path
-dataset = "mjshri23/life-expectancy-and-socio-economic-world-bank"
-download_path = "data"
-
-# Download dataset using Kaggle API
-kaggle.api.dataset_download_files(dataset, path=download_path, unzip=True)
-
-# Verify download by listing files
-print("Files downloaded to 'data' directory:")
-print(os.listdir(download_path))
-
-import pandas as pd
-file_path = os.path.join(download_path, "life-expectancy-data.csv")  # Adjust filename as needed
-df = pd.read_csv(file_path)
-print(df.head())
+#  Here ['life expectancy.csv']
+life_df = pd.read_csv('Life expectancy.csv')
+life_df
